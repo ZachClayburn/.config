67f84366e9e5fba92fc445b63c73e87b5435b03c
Zach Clayburn <zachclayburn@gmail.com>
2021-09-11 12:11:28 -0600

    Added configuration to neoscroll

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -273,7 +273,25 @@ local f =  require'packer'.startup(function(use)
 
   use { 'karb94/neoscroll.nvim',
     config = function()
-      require('neoscroll').setup()
+      require('neoscroll').setup{
+        hide_cursor = true,
+        stop_eof = true,
+        use_local_scrolloff = false,
+        respect_scrolloff = true,
+        cursor_scrolls_alone = true,
+        easing_function = 'sine',
+        pre_hook = nil,
+        post_hook = nil,
+      }
+      local mappings = {}
+      mappings['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '300'}}
+      mappings['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '300'}}
+      mappings['<C-Up>']   = {'scroll', {'-0.10', 'false', '100'}}
+      mappings['<C-Down>'] = {'scroll', { '0.10', 'false', '100'}}
+      mappings['zz'] = {'zz', {'250'}}
+      mappings['zt'] = {'zt', {'250'}}
+      mappings['zb'] = {'zb', {'250'}}
+      require('neoscroll.config').set_mappings(mappings)
     end
   }
 
