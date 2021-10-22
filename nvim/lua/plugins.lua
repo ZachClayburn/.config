d9110f0434f4a7666eeecb3fd78fb1f98f8e0782
Zach Clayburn <zachclayburn@gmail.com>
2021-10-22 11:33:39 -0600

    Added some more colorschemes

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -70,16 +70,6 @@ local f =  require'packer'.startup(function(use)
     end
   }
 
-  use{ 'folke/tokyonight.nvim',
-    config = function()
-      vim.g.tokyonight_style = 'storm'
-      vim.g.tokyonight_italic_functions = true
-      vim.g.tokyonight_italic_variables = true
-      vim.g.tokyonight_sidebars = { 'nerdtree', 'packer' }
-      vim.cmd "colorscheme tokyonight"
-    end
-  }
-
   use { 'NTBBloodbath/galaxyline.nvim',
     requires = { "kyazdani42/nvim-web-devicons", opt = true },
     config = function()
@@ -322,6 +312,45 @@ local f =  require'packer'.startup(function(use)
       require('rust-tools').setup{}
     end
   }
+
+  -- Color schemes
+  use { 'folke/tokyonight.nvim',
+    config = function()
+      vim.g.tokyonight_style = 'storm'
+      vim.g.tokyonight_italic_functions = true
+      vim.g.tokyonight_italic_variables = true
+      vim.g.tokyonight_sidebars = { 'nerdtree', 'packer' }
+      -- vim.cmd "colorscheme tokyonight"
+
+    end
+  }
+
+  use { 'rafamadriz/neon',
+    config = function()
+      -- Supports lualine
+      vim.g.neon_style = 'default'
+      vim.g.neon_italic_comment = true
+      vim.g.neon_italic_keyword = true
+      vim.g.neon_italic_boolean = true
+      vim.g.neon_italic_function = true
+      vim.g.neon_italic_variable = true
+      vim.g.neon_bold = true
+      vim.g.neon_transparent = false
+    end
+  }
+
+  use { 'bluz71/vim-nightfly-guicolors',
+    config = function()
+      -- supports lightline, vim-airline, lualine & vim-moonfly-statusline
+      -- has an alacritty theme
+      vim.g.nightflyCursorColor = 1
+    end
+  }
+
+  use { 'nxvu699134/vn-night.nvim'
+    -- supports galaxyline
+  }
+
 end)
 
 return f
